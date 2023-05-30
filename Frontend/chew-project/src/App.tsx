d727f29d809c0675d1c020c3d827c1e4772ef8a0
Tommy <Tommy.ba95@gmail.com>
2023-05-30 14:06:59 +0200

    div

--- Frontend/chew-project/src/App.tsx
+++ Frontend/chew-project/src/App.tsx
@@ -1,10 +1,20 @@
 
 import './App.css';
+import { HomePage } from './pages/HomePage';
+import { MyFrigde } from './pages/MyFrigde';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-   
+      <Router> 
+        <Navbar />
+          <Routes>
+            <Route path="Home" element={ <HomePage/> }></Route>
+            <Route path="Add" element={ <MyFrigde/> }></Route>
+          </Routes>
+      </Router>
+        <HomePage />
     </div>
   );
 }
--- Frontend/chew-project/src/pages/HomePage.tsx
+++ Frontend/chew-project/src/pages/HomePage.tsx
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 import React from 'react';
 import Navbar from '../components/Navbar';
+=======
+
+>>>>>>> 0282444 (div)
 
 export const HomePage = () => {  
     return (

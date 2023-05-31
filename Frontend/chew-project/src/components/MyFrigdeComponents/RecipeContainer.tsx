42403617d4833885f70c1aa918e634a3c0a85940
Tommy <Tommy.ba95@gmail.com>
2023-05-31 11:20:11 +0200

    RecipePage

---
+++ Frontend/chew-project/src/components/MyFrigdeComponents/RecipeContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/RecipeContainer.css"
+
+
+export const RecipeContainer  = () => {
+
+    return(
+        <div className="recipe-container">
+            <h1>Container </h1>
+        </div>
+    )
+}
--- Frontend/chew-project/src/pages/RecipePage.tsx
+++ Frontend/chew-project/src/pages/RecipePage.tsx
@@ -1,10 +1,16 @@
 import Navbar from "../components/Navbar"
 import { Link } from 'react-router-dom';
+import { MyFrigdeTitleBar } from "../components/MyFrigdeComponents/MyFrigdeTitleBar";
+import { RecipeContainer } from "../components/MyFrigdeComponents/RecipeContainer";
 
 export const RecipePage = () => {  
     return (
         <> 
-            <Navbar avatarSrc="default-avatar.png"/>        
+            <Navbar avatarSrc="default-avatar.png"/>   
+
+            <MyFrigdeTitleBar />     
+
+            <RecipeContainer />
 
             <Link to="/MyFrigde">MyFridge</Link>       
         </>

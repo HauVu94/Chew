e3cd4e3ea0d941770a0c3c3f21d9b0fbacb3f35b
Tommy <Tommy.ba95@gmail.com>
2023-05-31 10:47:42 +0200

    MyFrigdeComponents

---
+++ Frontend/chew-project/src/components/MyFrigdeComponents/MyFrigdeContainer.tsx
@@ -0,0 +1,8 @@
+export const MyFrigdeContainer  = () => {
+
+    return(
+        <div className="container">
+                <h1>Container</h1>
+        </div>
+    )
+}
--- Frontend/chew-project/src/pages/MyFridge.tsx
+++ Frontend/chew-project/src/pages/MyFridge.tsx
@@ -1,17 +1,17 @@
+import { MyFrigdeTitleBar } from "../components/MyFrigdeComponents/MyFrigdeTitleBar"
+import { MyFrigdeContainer } from "../components/MyFrigdeComponents/MyFrigdeContainer"
 import Navbar from "../components/Navbar"
 
 export const MyFridge = () => {  
     return (
         <> 
             <Navbar avatarSrc="default-avatar.png"/>   
-            <div className="page-title">
-                <h2>Knapp</h2>
-                <h2>Ditt Kjøleskap</h2>
-                <h2>Knapp</h2>
-            </div>
-            <div className="container">
-                <h1>Container</h1>
-            </div>
+
+            <MyFrigdeTitleBar />
+
+            <MyFrigdeContainer />
+
+            
         </>
     )
 }

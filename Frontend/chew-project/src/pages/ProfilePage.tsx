0cbbdf240496c34d4fff04702c858244206e4f62
schinon <banzai98@hotmail.com>
2023-06-01 16:36:44 +0200

    Profil begynt med xp system

--- Frontend/chew-project/src/pages/ProfilePage.tsx
+++ Frontend/chew-project/src/pages/ProfilePage.tsx
@@ -15,6 +15,18 @@ export const ProfilePage = () => {
                     <label>Navn: Ola Nordmann</label>
                     <label>Skole: Norges kuleste skole</label>
                 </div>
+                <div className='fremgang'></div>
+                <div className='tracker'></div>
+                <div className='bragder'>
+                    <h2>Bragder</h2>
+                    <div className='bragde-boks'>
+                        <div className='bragd-icon'></div>
+                        <div className='bragd-icon'></div>
+                        <div className='bragd-icon'></div>
+                        <div className='bragd-icon'></div>
+                        <div className='bragd-icon'></div>
+                    </div>
+                </div>
             </div>
 
             <img className="background-blue" src="../images/Dividerblue.png"></img>

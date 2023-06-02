ce3325cb652ecebb77553b082d5e358110a7bf6b
guillotinegoose <haavar.c.karlsen@gmail.com>
2023-06-02 11:10:11 +0200

    retrieve user by username - Make username unique in create_db.sql

--- Backend/Python_Files/userDao.py
+++ Backend/Python_Files/userDao.py
@@ -4,23 +4,33 @@ class UserDao:
 
     def save(self, user):
 
-        tupUser = (user.firstName, user.lastName, user.password)
+        tupUser = (user.firstName, user.lastName, user.username, user.password)
 
-        sqlStatement ="INSERT INTO User (firstName, lastName, password) VALUES (%s, %s, %s)"
+        sqlStatement ="INSERT INTO User (firstName, lastName, username, password) VALUES (%s, %s, %s, %s)"
 
         db_connector.mycursor.execute(sqlStatement, tupUser)
         db_connector.chewDb.commit()
 
 
-    def retrive(self, id):
+    def retriveById(self, id):
+        tupId = (id, )
 
-        sqlStatement = "SELECT * FROM User"
+        sqlStatement = "SELECT * FROM User WHERE id = %s"
 
-        db_connector.mycursor.execute(sqlStatement)
+        db_connector.mycursor.execute(sqlStatement, tupId)
         result = db_connector.mycursor.fetchall()
 
         for row in result:
             print(row)
+
+
+    def retriveByUsername(self, username):
+        tupUserName = (username, )
+        sqlStatement = "SELECT * FROM User WHERE username = %s"
+
+        db_connector.mycursor.execute(sqlStatement, tupUserName)
+
+        return db_connector.mycursor.fetchall().pop()
 
 
 

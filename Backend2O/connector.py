045181aab97c5b43fea4ae242cf869548e008b00
guillotineGoose <haavar.c.karlsen@gmail.com>
2023-06-03 15:38:49 +0200

    Update

---
+++ Backend2O/connector.py
@@ -0,0 +1,31 @@
+from mysql import connector
+import json
+from pathlib import Path
+
+# Gets password from
+"""
+    1. Create txt file named password.txt
+    2. Add your password on the first line
+    3. REMEMBER TO 
+        A. RIGHT CLICK
+        B. GO TO GIT
+        C. ADD TO GIT IGNORE
+"""
+password = Path("password.txt").open().readline().strip()
+print(password)
+
+
+def ConnectToDB():
+    Db = connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password=password,
+        auth_plugin='mysql_native_password',
+        database='test'
+    )
+    return Db
+
+
+t = ConnectToDB()
+
+print(t)

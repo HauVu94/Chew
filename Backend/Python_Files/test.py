263d22f88513b96bf1cc915d37713005c2dfbb5b
guillotinegoose <haavar.c.karlsen@gmail.com>
2023-06-03 14:51:33 +0200

    retrieve user by username - Make username unique in create_db.sql

--- Backend/Python_Files/test.py
+++ Backend/Python_Files/test.py
@@ -7,11 +7,9 @@ test = user.User("Ape", "Mamma", "ApeBaby665", 66644)
 
 dao = userDao.UserDao()
 
-dao.save(test)
 print(test)
 
 user = dao.retriveByUsername("ApeBaby665")
-dao.retriveById(1)
 
 print(user)
 

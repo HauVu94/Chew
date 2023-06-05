451b35c7baa5d416c6d3cb3f6342c497b5f93d62
Audun Slungård <ikkedytt@yahoo.no>
2023-06-05 12:53:26 +0200

    Search page finalized
    SPør meg om du får følgefeil, koz Audun <3

--- Frontend/chew-project/src/pages/IngredientSearchPage.tsx
+++ Frontend/chew-project/src/pages/IngredientSearchPage.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-// import Navbar from '../components/Navbar';
+import Navbar from '../components/NavigationComponents/Navbar';
 import Sidetabs from '../components/Sidetabs';
 import { Link } from 'react-router-dom';
 // import Toolbar from '../components/Toolbar';
 import IngredientSearch from '../components/SearchComponents/IngredientSearch';
 import '../styles/IngredientSearch.css'
-// import IngredientSearchResults from '../components/SearchComponents/IngredientSearchResults';
 
 const IngredientSearchPage = () => {
 

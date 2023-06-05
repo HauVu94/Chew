655e9fd7b5b978953ca231ade61122b8132fc7d1
Cinderus <marcus.eng2000@gmail.com>
2023-06-05 14:13:15 +0200

    Added connection support for user.py

--- Backend/SQLite/Database_Tables/user.py
+++ Backend/SQLite/Database_Tables/user.py
@@ -1,7 +1,24 @@
-class User:
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chew.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(30))
+    lastName = db.Column(db.String(30))
+    username = db.Column(db.String(30))
+    password = db.Column(db.String(50))
+
     def __init__(self, firstName, lastName, username, password):
-        #public members
         self.firstName = firstName
         self.lastName = lastName
         self.username = username
         self.password = password
+
+
+if __name__ == '__main__':
+    app.run()

799676526ca4b7bea7bfaba47d2ea65788c77749
Tommy <Tommy.ba95@gmail.com>
2023-06-05 09:29:19 +0200

    social and calendar

--- Frontend/chew-project/src/components/Sidetabs.tsx
+++ Frontend/chew-project/src/components/Sidetabs.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import * as BiIcon from "react-icons/bi"
-
+import { Link } from "react-router-dom";
 import "../styles/Sidetab.css"
+import { Calender } from "./PageComponents/Calender";
+import { Social } from "./PageComponents/Social";
 
 const Sidetabs = () => {
 
     return(
         <div className="sidetab-container">
             <div className="tab-wrapper">
-                <div className="sidetab sidetab-left">
-                    <BiIcon.BiCalendar />
-                </div>
-                <div className="sidetab sidetab-right">
-                    <BiIcon.BiUser />
-                </div>
+                
+                <Calender />
+
+                <Social />
             </div>
         </div>
     )

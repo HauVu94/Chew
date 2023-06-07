b9dd4455e21b5ccfd67aa6981c8610d6f09c520e
guillotineGoose <haavar.c.karlsen@gmail.com>
2023-06-07 10:46:26 +0200

    npm run dev

--- Frontend/server/server.js
+++ Frontend/server/server.js
@@ -2,6 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/login", (req, res) => {
+  return res.json({ username: "Haavar123", fullName: "Håvar Cedolf Karlsen" });
+});
+
+
+app.use(express.static("../client/dist"))
+app.use((req, res) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+
+
+
+
 const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Listening on http://localhost:${server.address().port}`);
 });

0fb520d3bd2f4bcd039b58c0eed49bc67db8c30a
schinon <banzai98@hotmail.com>
2023-06-08 15:05:25 +0200

    Level progression

--- Frontend/client/src/pages/ProfilePage.tsx
+++ Frontend/client/src/pages/ProfilePage.tsx
@@ -4,7 +4,7 @@ import ProfileWidget from '../components/ProfileWidgets/ProfileWidget';
 import ProgressionBar from '../components/ProfileWidgets/ProgressionBar';
 import Toolbar from '../components/Toolbar';
 import "../styles/Profilepage.css";
-import "../styles/Progression.css"
+// import "../styles/Progression.css";
 
 export const ProfilePage = () => {  
     return (

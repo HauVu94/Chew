782e7eaef6fe6754af42fe69a614273bcaed412d
paaulsens <nikko_herre@outlook.com>
2023-06-08 20:24:48 +0200

    search

--- Frontend/client/src/pages/IngredientSearchPage.tsx
+++ Frontend/client/src/pages/IngredientSearchPage.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Navbar from '../components/NavigationComponents/Navbar';
 import Sidetabs from '../components/Sidetabs';
+import { PageTitle } from '../components/PageComponents/PageTitle';
 import { Link } from 'react-router-dom';
 // import Toolbar from '../components/Toolbar';
 import IngredientSearch from '../components/SearchComponents/IngredientSearch';
+import { SearchSidetab } from '../components/SearchComponents/SearchSidetab';
 import '../styles/IngredientSearch.css'
 
 const IngredientSearchPage = () => {
@@ -13,10 +15,12 @@ const IngredientSearchPage = () => {
 
     return (
         <>
-            <Navbar avatarSrc={require("../../public/images/profile.png")}/>
-            <Sidetabs />
-
-            <IngredientSearch />
+        
+        <Navbar avatarSrc={require("../../public/images/profile.png")}/>
+        <SearchSidetab/>
+        <PageTitle pageTitle="Legg til i kjøleskap"/>
+        <IngredientSearch />
+        
         </>
     )
 }
